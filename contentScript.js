acd19e8965871b6639d8cda1f72c6bd4422cacfd
stan-voo <stasparshin@gmail.com>
2023-03-19 21:00:29 +0300

    Output customization via Options page

--- contentScript.js
+++ contentScript.js
@@ -1,20 +1,21 @@
 function copySelectedTextAsMarkdown() {
-    const selectedText = window.getSelection().toString();
-    const cleanedSelectedText = selectedText.replace(/[\r\n]+/g, ' ');
-    const pageTitle = document.title;
-    const pageUrl = window.location.href;
-    const markdown = `> ${cleanedSelectedText} | [src](${pageUrl})`;
-  
-    // Copy the markdown to the clipboard
+  const selectedText = window.getSelection().toString().replace(/[\r\n]+/g, " ");
+  const pageUrl = window.location.href;
+
+  chrome.storage.sync.get("format", (data) => {
+    const format = data.format || "> [selected text]) | [src]([current page URL])";
+    const markdown = format.replace("[selected text]", selectedText).replace("[current page URL]", pageUrl);
+
     navigator.clipboard.writeText(markdown).then(
       () => {
-        console.log('Markdown copied to clipboard:', markdown);
+        console.log("Markdown copied to clipboard:", markdown);
       },
       (err) => {
-        console.error('Failed to copy text:', err);
+        console.error("Failed to copy text:", err);
       }
     );
-  }    
+  });
+}
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'copyMarkdown') {
       copySelectedTextAsMarkdown();
---
+++ options.js
@@ -0,0 +1,34 @@
+const defaultFormat = "> [selected text]) | [src]([current page URL])";
+const outputFormatElement = document.getElementById("output-format");
+const saveButton = document.getElementById("save-format");
+const messageElement = document.getElementById("message"); // Add this line
+
+function showMessage(text, isError = false) {
+  messageElement.textContent = text;
+  messageElement.style.color = isError ? "red" : "green";
+  setTimeout(() => {
+    messageElement.textContent = "";
+  }, 3000);
+}
+
+function saveOptions() {
+  const format = outputFormatElement.value;
+  chrome.storage.sync.set({ format }, () => {
+    if (chrome.runtime.lastError) {
+      console.error("Error:", chrome.runtime.lastError.message);
+      showMessage("Failed to save output format. Please try again.", true);
+    } else {
+      console.log("Output format saved:", format);
+      showMessage("Output format saved successfully.");
+    }
+  });
+}
+
+function restoreOptions() {
+  chrome.storage.sync.get("format", (data) => {
+    outputFormatElement.value = data.format || defaultFormat;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+saveButton.addEventListener("click", saveOptions);
